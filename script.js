03eccd9f2627a12f45e1d4f8c3e65b7e4b67bd73
elshali <114364318+elshali@users.noreply.github.com>
2022-10-16 03:09:13 -0400

    finishing UI, implementing js

---
+++ script.js
@@ -0,0 +1,27 @@
+const form = document.querySelector('form');
+const name = document.querySelector('#firstName');
+
+const submitBtn = document.querySelector('#submit');
+const cancelBtn = document.querySelector('#cancel');
+
+const h5 = document.querySelector('h5');
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+});
+
+submitBtn.addEventListener('click', function() {
+    localStorage.setItem('name', name.value);
+
+    nameDisplayCheck();
+
+});
+
+function nameDisplayCheck() {
+    if (localStorage.getItem('name')) {
+        let name = localStorage.getItem('name');
+        console.log("hello");
+        h5.textContent = `please work`;
+        
+    }
+}
